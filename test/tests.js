fda924ec29ec7d8905ed81591adef66e68a8f834
W. Andrew Loe III <andrew@andrewloe.com>
2014-07-09 10:20:57 -0700

    Build tests.

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,5 @@
  * DON'T EDIT THIS FILE. It's automatically generated!
  */
 
-require('../lib/.DS_Store/test.js');
 require('../lib/google-analytics/test.js');
 require('../lib/optimizely/test.js');
